e2f662eff66129b20dd482f1e70d5ae33a3981bd
Gregory Keeley <gregorykeeley@pursuit.org>
2019-11-16 17:43:26 -0500

    - Added UI Elements - Updated Win/Loss condition

--- SwitchOff/ViewController.swift
+++ SwitchOff/ViewController.swift
@@ -9,32 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var switch_1: UISwitch!
     @IBOutlet weak var switch_2: UISwitch!
     @IBOutlet weak var switch_3: UISwitch!
-    
     @IBOutlet weak var winLoseLabel: UILabel!
+    @IBOutlet weak var switchCountLabel: UILabel!
+    
+    var switchCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        switchCountLabel.text = "0"
     }
     func switchCheck() {
-        if switch_1.isOn == false && switch_2.isOn == false && switch_3.isOn == false {
+        if switchCount > 25 {
+            if switch_1.isOn == false && switch_2.isOn == false && switch_3.isOn == false {
             winLoseLabel.text = "WIN!"
-        } else {
-            winLoseLabel.text = "Lose!"
+            } else {
+                winLoseLabel.text = "Lose!"
+                switch_1.isEnabled = false
+                switch_2.isEnabled = false
+                switch_3.isEnabled = false
+            }
         }
     }
     @IBAction func switch_1Toggled(_ sender: UISwitch) {
         switch_2.isOn = true
         switch_3.isOn = false
+        switchCount += 1
+        switchCountLabel.text = String(switchCount)
+        switchCheck()
     }
     @IBAction func switch_2Toggled(_ sender: UISwitch) {
         switch_1.isOn = false
         switch_3.isOn = true
+        switchCount += 1
+        switchCountLabel.text = String(switchCount)
+        switchCheck()
     }
     @IBAction func switch_3Toggled(_ sender: UISwitch) {
         if switch_2.isOn == true {
@@ -42,8 +55,11 @@ class ViewController: UIViewController {
         }
         switch_2.isOn = true
         switch_1.isOn = false
+        switchCount += 1
+        switchCountLabel.text = String(switchCount)
+        switchCheck()
     }
     
-
+    
 }
 
